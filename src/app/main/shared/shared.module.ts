b5ab88918d6b66ca3f79092e331c7f47ca10957c
priya_balakrishnan <priya.balakrishnan@prutech.com>
2022-02-17 13:52:53 -0500

    file upload

--- src/app/main/shared/shared.module.ts
+++ src/app/main/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { CustomModalComponent } from './custom-modal/custom-modal.component';
 import { NgModule } from '@angular/core';
 import { CoreCommonModule } from '@core/common.module';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 
 @NgModule({
-  declarations: [CustomModalComponent],
+  declarations: [CustomModalComponent, FileUploadComponent],
   imports: [CoreCommonModule, NgSelectModule],
-  exports: [CustomModalComponent],
+  exports: [CustomModalComponent, FileUploadComponent],
   providers: []
 })
 export class SharedModule {}
